1c69bd84259166d6b87a9a92039732b67b514517
ales <dev.ales.k@gmail.com>
2017-10-01 19:16:55 +0200

    fix

--- email.go
+++ email.go
@@ -9,7 +9,10 @@ import (
 func (c *Context) SendEmail(message *mail.Message, t *template.Template, data interface{}) error {
 	buf := new(bytes.Buffer)
 	defer buf.Reset()
-	t.ExecuteTemplate(buf, "email", data)
+	err := t.ExecuteTemplate(buf, "email", data)
+	if err != nil {
+		return err
+	}
 	message.HTMLBody = buf.String()
 	return mail.Send(c.Context, message)
 }
