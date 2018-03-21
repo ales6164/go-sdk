77b2d392ffaac50e57106e52dfd48ce674fb3427
Aleš <dev.ales.k@gmail.com>
2018-03-21 13:57:07 +0100

    origin

--- sdk.go
+++ sdk.go
@@ -40,7 +40,7 @@ var signingKey []byte
 
 func (s *MyServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if origin := req.Header.Get("Origin"); origin != "" {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers",
 			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Cache-Control, "+
